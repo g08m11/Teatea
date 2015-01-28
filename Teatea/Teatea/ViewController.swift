23e466ac6362aeaf44f53df1092eb2a5a5442343
= <=>
2015-01-29 01:40:08 +0900

    一時コミット

--- Teatea/Teatea/ViewController.swift
+++ Teatea/Teatea/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var weatherView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    let weatherImage = UIImage(named: "t1.jpg")
+    weatherView.image = weatherImage
+    self.view.addSubview(weatherView)
   }
 
   override func didReceiveMemoryWarning() {
--- Teatea/Teatea/ZazenViewController.swift
+++ Teatea/Teatea/ZazenViewController.swift
@@ -10,26 +10,77 @@ import UIKit
 
 class ZazenViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  //時間計測用の変数.
+  var cnt : Float = 0
+  
+  //時間表示用のラベル.
+  var myLabel : UILabel!
+  
+  //タイマー.
+  var timer : NSTimer!
+  
+  override func viewDidLoad() {
+    
+    //ラベルを作る.
+    myLabel = UILabel(frame: CGRectMake(0,0,200,50))
+    myLabel.backgroundColor = UIColor.grayColor()
+    myLabel.layer.masksToBounds = true
+    myLabel.layer.cornerRadius = 20.0
+    myLabel.text = "Time:\(cnt)"
+    myLabel.textColor = UIColor.whiteColor()
+    myLabel.shadowColor = UIColor.grayColor()
+    myLabel.textAlignment = NSTextAlignment.Center
+    myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+    self.view.backgroundColor = UIColor.whiteColor()
+    self.view.addSubview(myLabel)
+    
+    //タイマー停止ボタンを作る.
+    let myButton = UIButton(frame: CGRectMake(0, 0, 200, 50))
+    myButton.layer.masksToBounds = true
+    myButton.layer.cornerRadius = 20.0
+    myButton.backgroundColor = UIColor.grayColor()
+    myButton.setTitle("Zazen Stop", forState: UIControlState.Normal)
+    myButton.layer.position = CGPointMake(self.view.center.x, self.view.center.y + 100)
+    myButton.addTarget(self, action: "onMyButtonClick:", forControlEvents: UIControlEvents.TouchUpInside)
+    self.view.addSubview(myButton)
+    
+    //タイマーを作る.
+    timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
+  }
+  
+  //ボタンが押された時に呼ばれるメソッド.
+  func onMyButtonClick(sender : UIButton){
+    
+    //timerが動いてるなら.
+    if timer.valid == true {
+      
+      //timerを破棄する.
+      timer.invalidate()
+      
+      //ボタンのタイトル変更.
+      sender.setTitle("Zazen Start", forState: UIControlState.Normal)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    else{
+      
+      //timerを生成する.
+      timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
+      
+      //ボタンのタイトル変更.
+      sender.setTitle("Zazen Stop", forState: UIControlState.Normal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  //NSTimerIntervalで指定された秒数毎に呼び出されるメソッド.
+  func onUpdate(timer : NSTimer){
+    
+    cnt += 0.1
+    
+    //桁数を指定して文字列を作る.
+    let str = "Time:".stringByAppendingFormat("%.1f",cnt)
+    
+    myLabel.text = str
+    
+  }
+  
 }
