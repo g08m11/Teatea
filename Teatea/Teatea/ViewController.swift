7c646c0091fd3d3349a7019d493a53e2a1d5df59
= <=>
2015-01-29 01:26:41 +0900

    一時コミット

---
+++ Teatea/Teatea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teatea
+//
+//  Created by 具志堅 雅 on 2015/01/28.
+//  Copyright (c) 2015年 Bloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
